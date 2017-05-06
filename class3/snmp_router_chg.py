7175ea2c483fb61f76f40df0e8b50e26d5ce34c6
Chip Hudgins <chudgins@salesforce.com>
2017-05-05 21:51:43 -0700

    updates

--- class3/snmp_router_chg.py
+++ class3/snmp_router_chg.py
@@ -45,11 +45,12 @@ RUN_LAST_CHANGED = '1.3.6.1.4.1.9.9.43.1.1.1.0'
 # 300 seconds (converted to hundredths of seconds)
 RELOAD_WINDOW = 300 * 100
 
-# YAML or PICKLE file?
-# 'yaml_file' or 'pickle_file'
+# YAML or PICKLE file? 'yaml_file' or 'pickle_file'
 FILE_TYPE = 'yaml_file'
 
-def load_file(filepath):
+DEBUG = True
+
+def load_devices(filepath):
     if os.path.isfile(filepath):
         with open(filepath, "r") as file:
             if FILE_TYPE == 'yaml_file':
@@ -58,13 +59,14 @@ def load_file(filepath):
                 router_data = pickle.load(file)
             file.close()
 
+        print "{0} devices were previously saved\n".format(len(router_data))
         return router_data
 
     else:
-        print "No file {} to load.".format(filepath)
-        return None
+        print "No devices in {} to load.".format(filepath)
+        return {}
 
-def save_file(device, device_data, filepath):
+def save_device(device, device_data, filepath):
     data = load_file(filepath)
     data[device] = device_data
 
@@ -74,15 +76,15 @@ def save_file(device, device_data, filepath):
         else:
             pickle.dump(data, file)
 
-def has_router_changed(snmp_data, router_data):
-    if snmp_data['last_changed'] > router_data['last_changed']:
-        return True
-    elif snmp_data['uptime'] < router_data['uptime']:
-        # reload has occurred
-        # check if changed has happened since reload
-        if snmp_data['last_changed'] >= RELOAD_WINDOW:
-            return True
-    return False
+# def has_router_changed(snmp_data, router_data):
+#     if snmp_data['last_changed'] > router_data['last_changed']:
+#         return True
+#     elif snmp_data['uptime'] < router_data['uptime']:
+#         # reload has occurred
+#         # check if changed has happened since reload
+#         if snmp_data['last_changed'] >= RELOAD_WINDOW:
+#             return True
+#     return False
 
 def mail_notification(device):
     recipient = 'mudzi42@gmail.com'
@@ -104,17 +106,34 @@ def mail_notification(device):
     except Exception, e:
         print("Email notification failed to send for {} with error: {}.".format(device, e))
 
-def run_first_time(filepath):
-    if not os.path.isfile(filepath):
-        data = {'pynet_rtr2': {'uptime': 0, 'last_changed': 0},
-                'pynet_rtr1': {'uptime': 0, 'last_changed': 0}}
+#     '''
+#     Send email notification regarding modified device
+#     '''
+#
+#     current_time = datetime.now()
+#
+#     sender = 'sender@twb-tech.com'
+#     recipient = 'recipient@twb-tech.com'
+#     subject = 'Device {0} was modified'.format(net_device.device_name)
+#
+#     message = '''
+# The running configuration of {0} was modified.
+# This change was detected at: {1}
+# '''.format(net_device.device_name, current_time)
+#
+#     if send_mail(recipient, subject, message, sender):
+#         print 'Email notification sent to {}'.format(recipient)
+#         return True
 
-        with open(filepath, 'w') as file:
-            if FILE_TYPE == 'yaml_file':
-                yaml.dump(data, file, default_flow_style=False)
-            else:
-                pickle.dump(data, file)
-
+# def get_snmp_data(snmp_user, routers):
+#     snmp_extract_output = {}
+#     for a_device in routers:
+#         for the_oid in (SYS_NAME, SYS_UPTIME, RUN_LAST_CHANGED):
+#             snmp_data = snmp_get_oid_v3(a_device, snmp_user, oid=the_oid)
+#             snmp_extract_output.append(snmp_extract(snmp_data))
+#     return snmp_extract_output
+#     #return {'pynet_rtr2': {'uptime': 2445540900, 'last_changed': 3045457714},
+#     #                       'pynet_rtr1': {'uptime': 2575100104, 'last_changed': 3045681252}}
 
 def main():
     a_user = 'pysnmp'
@@ -129,24 +148,62 @@ def main():
     pynet_rtr1 = ('184.105.247.70', 161)
     pynet_rtr2 = ('184.105.247.71', 161)
 
+    print "Checking for router changes ..."
+    saved_devices = load_devices(router_file)
+
+    # # Temporarily store the current devices in a dictionary
+    current_devices = {}
+
+    # Connect to each device / retrieve last_changed time
     for a_device in (pynet_rtr1, pynet_rtr2):
+        snmp_results = []
+        for oid in (SYS_NAME, SYS_UPTIME, RUN_LAST_CHANGED):
+            try:
+                value = snmp_extract(snmp_get_oid_v3(a_device, snmp_user, oid=oid))
+                snmp_results.append(int(value))
+            except ValueError:
+                snmp_results.append(value)
+        device_name, uptime, last_changed = snmp_results
+        if DEBUG:
+            print "\nConnected to device = {0}".format(device_name)
+            print "Last changed timestamp = {0}".format(last_changed)
+            print "Uptime = {0}".format(uptime)
 
-        for the_oid in (SYS_NAME, SYS_UPTIME, RUN_LAST_CHANGED):
-            snmp_data = snmp_get_oid_v3(a_device, snmp_user, oid=the_oid)
-            snmp_extract_output = snmp_extract(snmp_data)
+        # see if this device has been previously saved
+        if device_name in saved_devices:
+            saved_device = saved_devices[device_name]
+            print "{0} {1}".format(device_name, (35 - len(device_name))*'.'),
 
-        #snmp_extract_output = {'pynet_rtr2': {'uptime': 2445540900, 'last_changed': 3045457714},
-        #                        'pynet_rtr1': {'uptime': 2575100104, 'last_changed': 3045681252}}
-        run_first_time(router_file)
-        router_info = load_file(router_file)
-
-        for a_device in snmp_extract_output:
-            if has_router_changed(snmp_extract_output[a_device], router_info[a_device]):
-                mail_notification(a_device)
-                save_file(a_device, snmp_extract_output[a_device], router_file)
+            # Check for a reboot (did uptime decrease or last_changed decrease?)
+            if uptime < saved_device.uptime or last_changed < saved_device.last_changed:
+                if last_changed <= RELOAD_WINDOW:
+                    print "DEVICE RELOADED...not changed"
+                else:
+                    print "DEVICE RELOADED...and changed"
+                    save_device(device_name, {'uptime': uptime, 'last_changed': last_changed}, router_file)
+                    mail_notification(current_devices[device_name])
+            elif last_changed == saved_device.last_changed:
+                # running-config last_changed is the same
+                print "not changed"
+            elif last_changed > saved_device.last_changed:
+                # running-config was modified
+                print "CHANGED"
+                save_device(device_name, {'uptime': uptime, 'last_changed': last_changed}, router_file)
+                mail_notification(current_devices[device_name])
             else:
-                print "No configuration changes detected on {}\n".format(a_device)
+                raise ValueError()
+        else:
+            # New device, just save it
+            print "{0} {1}".format(device_name, (35 - len(device_name))*'.'),
+            print "saving new device"
+            save_device(device_name, {'uptime': uptime, 'last_changed': last_changed}, router_file)
 
+    # Write the devices to pickle file
+    with open(net_dev_file, 'w') as f:
+        for dev_obj in current_devices.values():
+            pickle.dump(dev_obj, f)
+
+    print
 
 if __name__ == "__main__":
     main()
