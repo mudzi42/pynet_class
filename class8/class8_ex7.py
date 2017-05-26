0f1c059dfe58808625dd205d84404d5e8f0f0272
Chip Hudgins <chudgins@salesforce.com>
2017-05-26 14:45:38 -0700

    ex7

--- class8/class8_ex7.py
+++ class8/class8_ex7.py
@@ -9,23 +9,40 @@ __email__ = 'mudzi42@gmail.com'
 
 import django
 from net_system.models import NetworkDevice, Credentials
+from netmiko import ConnectHandler
+from datetime import datetime
+from multiprocessing import Process
+
+def show_version(a_device):
+    remote_conn = ConnectHandler(device_type=a_device.device_type,
+                                 ip=a_device.ip_address,
+                                 username=a_device.credentials.username,
+                                 password=a_device.credentials.password,
+                                 port=a_device.port, secret='')
+    print
+    print '#' * 80
+    print remote_conn.send_command_expect("show version")
+    print '#' * 80
+    print
 
 def main():
     django.setup()
+
+    start_time = datetime.now()
     net_devices = NetworkDevice.objects.all()
 
+    procs = []
     for a_device in net_devices:
-        if 'cisco' in a_device.device_type:
-            a_device.vendor = 'Cisco'
-        elif 'juniper' in a_device.device_type:
-            a_device.vendor = 'Juniper'
-        elif 'arista' in a_device.device_type:
-            a_device.vendor = 'Arista'
+        my_proc = Process(target=show_version, args=(a_device,))
+        my_proc.start()
+        procs.append(my_proc)
 
-        a_device.save()
+    for a_proc in procs:
+        print a_proc
+        a_proc.join()
 
-    for a_device in net_devices:
-        print a_device, a_device.device_type
+    elapsed_time = datetime.now() - start_time
+    print "Final elapsed time: {}".format(elapsed_time)
 
 if __name__ == '__main__':
     main()
